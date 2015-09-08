dc01756cdec3bec74597a539ff1eeca18f2c70e6
Miguel T Rivera <miguel@miguels-macbook.lan>
2015-09-08 01:56:48 -0700

    Default code

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Miguel T Rivera on 9/8/15.
+//  Copyright (c) 2015 self. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+}
---
+++ ExchangeAGram/FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  ExchangeAGram
+//
+//  Created by Miguel T Rivera on 9/8/15.
+//  Copyright (c) 2015 self. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class FeedItem: NSManagedObject {
+
+    @NSManaged var image: NSData
+    @NSManaged var caption: String
+
+}
