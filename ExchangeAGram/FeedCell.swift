d393b8fde1c8638d14c7e04c4bc5536153e420a8
Miguel T Rivera <miguel@miguels-macbook.lan>
2015-09-08 02:00:40 -0700

    ExchangeaGram

--- ExchangeAGram/FeedCell.swift
+++ ExchangeAGram/FeedCell.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
-class FeedCell: UICollectionViewCell {
-    
+class FeedCell: UICollectionViewCell
+{
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
 }
